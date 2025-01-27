a8c03019cf20aef959e7fd7370b9509ff30061de
Ankit-nZ635 <ankiit565@gmail.com>
2025-01-27 23:35:39 +0530

    js assignment

--- basic.js
+++ basic.js
@@ -1,2 +1,8 @@
-let ankit =document.querySelector('h1');
-ankit.textContent = "web devlopment";
+let zone =document.querySelector('h1');
+let bone =document.querySelector('h2');
+let cone =document.querySelector('p');
+
+zone.textContent = "web devlopment";
+bone.textContent = "first class of java script";
+cone.textContent = "i first impementation through js";
+
